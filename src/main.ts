d42dede78320e117f1f91208d684bd5cf02a17d6
amogh-chavan <chavanamogh1@gmail.com>
2024-03-26 17:27:11 +0530

    Using fastify for server

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './api/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(), //using fastify instead of express because it is 2x in speed
+  );
   await app.listen(3000);
 }
 bootstrap();
