385e2682c4999c563f5803fe7d4ef448625070e3
amogh-chavan <chavanamogh1@gmail.com>
2024-03-26 18:30:24 +0530

    swagger setup

---
+++ src/constant/message.constant.ts
@@ -0,0 +1,5 @@
+export const swaggerMessages = {
+  TITLE: 'ZAGNOW API',
+  DESCRIPTION:
+    'We are a discovery platform operating in the digital commerce space in India. We help consumers discover, connect and shop with millions of local products and services.',
+};
--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import {
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
 import { AppModule } from './module/app.module';
+import setupSwagger from './config/swagger.config';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
     new FastifyAdapter(), //using fastify instead of express because it is 2x in speed
   );
+
+  setupSwagger(app);
+
   await app.listen(3000);
 }
 bootstrap();
